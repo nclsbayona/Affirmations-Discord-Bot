83cd2aacb4b31c05fcc474cd663d99bb4045709e
nclsbayona <59931437+nclsbayona@users.noreply.github.com>
2021-07-12 23:04:15 +0000

    bot style

--- main.py
+++ main.py
@@ -1,78 +1,87 @@
 import os
 import discord
-import requests
-import random
-from replit import db
+from requests import get, Response
+from discord.ext import commands, tasks
+from random import randint
+# from replit import db
 from keep_web_alive import keep_alive
 
-client = discord.Client()
-my_secret = os.environ['TOKEN']
-my_words = ["!p", "!", "Hola", "Perro", '$af']
-db["random_quotes"] = [
-    "Everyone wants happiness, no one wants pain, but you can have a sunshine without a bit of rain",
-    "When you have gone as deep as you can go, the good thing is you can only go higher"
-]
+bot_token = os.environ['TOKEN']
+
+channel_id = os.environ['CHANNEL']
 
 
-def get_quote(number: int) -> str:
+class AffirmationsBot(commands.Bot):
+    def __init__(
+            self,
+            channel_id,
+            bot_prefix,
+            timing=2,
+            bot_description="Simple bot that sends affirmations every once in a while to certain channel",
+            bot_intents=discord.Intents.default()):
+        super().__init__(command_prefix=bot_prefix,
+                         description=bot_description,
+                         intents=bot_intents)
+        self.time = timing
+        self.chanel = None
+        self.channel_id = channel_id
+        self.postAffirmation.start()
+        self.clearChannel.start()
 
-	if (number == 1):
-		msg = (requests.get("https://www.affirmations.dev/").json().get(
-		    "affirmation"))
-		print("1")
-	elif (number == 2):
-		msg = requests.get("https://zenquotes.io/api/random").json()[0].get(
-		    "q")
-		print("2")
-	return msg
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def getAffirmation(self, amount=1):
+        affirmations = list()
+        response: Response = None
+        affirmation: str = None
+        for (i) in range(amount):
+            choices: int = randint(1, 3)
+            if choices == 1:
+                response = get("https://affirmations.dev")
+                affirmation = (response.json()).get("affirmation")
+
+            elif choices == 2:
+                response = get("https://zenquotes.io/api/random")
+                affirmation = (response.json())[0].get("q")
+
+            elif choices == 3:
+                response = get("https://quotes.rest/qod.json?language=en")
+                affirmation = ((response.json()).get("contents").get("quotes")
+                               [0].get("quote"))
+
+            affirmations.append(affirmation)
+        return affirmations
+
+    @tasks.loop(minutes=30)
+    async def postAffirmation(self):
+      aff=await self.getAffirmation()
+      await (self.channel).send(aff[0])
+
+    @tasks.loop(hours=48)
+    async def clearChannel(self):
+      try:
+        await (self.channel).purge(limit=100000)
+      except:
+        pass
+
+    @postAffirmation.before_loop
+    async def beforePosting(self):
+        await self.wait_until_ready()
+        self.channel = self.get_channel(self.channel_id)
+        await self.clearChannel()
 
 
-def update_random(quote: str):
-	if ("random_quotes" in db.keys()):
-		random_quotes = db["random_quotes"]
-		random_quotes.append(quote)
-		db["random_quotes"] = random_quotes
-	else:
-		db["random_quotes"] = list(random_quotes)
-
-
-def delete_random(index: int = 0):
-	random_quotes = db["random_quotes"]
-	if (len(random_quotes) > index):
-		quote = random_quotes[index]
-		del random_quotes[index]
-		db["random_quotes"] = random_quotes
-		return quote
-
-
-@client.event
-async def on_ready():
-	print("This '{0.user}' bot is now ready to start".format(client))
-
-
-@client.event
-async def on_message(message: discord.Message):
-	if (message.author == client.user):
-		return
-	if (message.content.split(" ", 1)[0] in my_words):
-		msg = get_quote(random.choice(list(range(1, 3))))
-		print(msg)
-		await message.channel.send(msg)
-	elif (message.content.startswith("random_quote")):
-		await message.channel.send(random.choice(db["random_quotes"]))
-	elif message.content.startswith(".add"):
-		db["random_quotes"].append(message.content.split(".add ", 1)[1])
-		await message.channel.send("Your quote was added\n" +
-		                           random.choice(db["random_quotes"]))
-	elif message.content.startswith(".del"):
-		try:
-			index = int(message.content.split(".del ", 1)[1])
-		except:
-			index = 0
-		quote = delete_random(index)
-		await message.channel.send("The quote {quote} was deleted\n".format(
-		    **locals()) + random.choice(db["random_quotes"]))
-
-
+bot = AffirmationsBot(int(channel_id), "..")
+@bot.command(name="get")
+async def gets(ctx, *, times:int):
+  try:
+      times=int(times)
+      aff=await bot.getAffirmation(amount=times)
+      for (i, affirmation) in enumerate(aff):
+        await (bot.channel).send('#'+str(i+1)+':\n'+affirmation)
+  except Exception as e:
+    bot.channel.send(e.args)
 keep_alive()
-client.run(my_secret)
+bot.run(bot_token)
